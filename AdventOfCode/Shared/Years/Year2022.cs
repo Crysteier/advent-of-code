c629b0cd76d2ca4a3da61c70569d13bdbd66b139
Adam Moricz <adam.moricz@gmail.com>
2023-12-02 14:17:14 +0100

    asd

---
+++ AdventOfCode/Shared/Years/Year2022.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Shared.Years
+{
+    public abstract class Year2022 : DaySolver
+    {
+        public Year2022(string day) : base($"Y2022//" + day) { }
+        public abstract string Solution1();
+        public abstract string Solution2();
+
+    }
+}
--- AdventOfCode/Y2022/Day1/Day1.cs
+++ AdventOfCode/Y2022/Day1/Day1.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AdventOfCode.Shared.Years;
 
 namespace AdventOfCode.Y2022.Day1
 {
-    public class Day1
+    public class Day1 : Year2022
     {
+        public Day1(string day) : base(day)
+        {
+        }
+
+        public override string Solution1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Solution2()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
